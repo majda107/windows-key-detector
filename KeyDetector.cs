b032aa0f64f3068cc7f215cbc3b81cb21751de0a
majda107 <majda.trpkos@gmail.com>
2019-08-23 16:13:36 +0200

    Added PInvokes

--- KeyDetector.cs
+++ KeyDetector.cs
@@ -8,7 +8,26 @@ namespace KeyDetectorNET
 {
     public class KeyDetector
     {
+        /* PInvokes */
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        /// <summary>
+        /// Detector thread
+        /// </summary>
         private Thread applicationThread;
+
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
 
