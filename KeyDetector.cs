89bc04ab864ade72c7730f757e1c64e388cf85e1
majda107 <majda.trpkos@gmail.com>
2019-08-23 16:10:57 +0200

    Project created

---
+++ KeyDetector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace KeyDetectorNET
+{
+    public class KeyDetector
+    {
+        private Thread applicationThread;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private delegate void KeyPressedEventHandler(object o, KeyPressedEventArgs e);
+        static private event KeyPressedEventHandler KeyPressed;
+        public KeyDetector()
+        {
+
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (KeyPressed != null)
+                {
+                    KeyPressed(null, new KeyPressedEventArgs((VirtualKeyStates)vkCode));
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (ProcessModule curModule = Process.GetCurrentProcess().MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+    }
+}
